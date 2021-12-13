52adf18ca7ba9023ff04b0d3d75bdea63fe906fa
tbwebb22 <tbwebb22@gmail.com>
2021-12-13 10:43:15 -0500

    Fix GameCard text z-index

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,16 +15,16 @@ function GameCard({
 
     return (
       <div className="flex w-64 h-44 m-3 bg-black rounded-xl cursor-pointer" onClick={() => openSmartContract(smartContract)} >
+        <div className="flex h-38">
+          <Image src={smartContract.imageUri} className="object-cover h-44 w-64 rounded-xl opacity-95" type="content" />
+        </div>
         <div className="flex flex-grow absolute justify-center h-6 w-64">
-          <div className="flex text-offWhite text-sm z-50 mt-1 h-6 truncate ...">
+          <div className="flex text-offWhite text-sm mt-1 h-6 truncate ...">
             {smartContract.name}
           </div>
         </div>
-        <div className="flex absolute text-offWhite text-sm z-50 mt-36 ml-4 h-6 truncate ...">
+        <div className="flex absolute text-offWhite text-sm mt-36 ml-4 h-6 truncate ...">
           {smartContract.creator}
-        </div>
-        <div className="flex h-38">
-          <Image src={smartContract.imageUri} className="object-cover h-44 w-64 rounded-xl opacity-95" type="content" />
         </div>
       </div>
     );
