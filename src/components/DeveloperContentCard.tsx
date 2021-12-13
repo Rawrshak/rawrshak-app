7562bcf7cec873d798a146ae60a1cda7b9518242
tbwebb22 <tbwebb22@gmail.com>
2021-12-13 10:46:47 -0500

    Fix DeveloperContentCard text z-index

--- src/components/DeveloperContentCard.tsx
+++ src/components/DeveloperContentCard.tsx
@@ -15,16 +15,16 @@ function DeveloperContentCard({
 
     return (
       <div className="flex w-64 h-44 m-3 bg-black rounded-xl cursor-pointer" onClick={() => openSmartContract(smartContract)} >
+        <div className="flex h-38">
+          <Image src={smartContract.imageUri} className="object-cover h-44 w-64 rounded-xl opacity-95" type="content" />
+        </div>
         <div className="flex flex-grow absolute justify-center h-6 w-64">
-          <div className="flex text-offWhite text-sm z-10 mt-1 h-6 truncate ...">
+          <div className="flex text-offWhite text-sm mt-1 h-6 truncate ...">
             {smartContract.name}
           </div>
         </div>
-        <div className="flex absolute text-offWhite text-sm z-10 mt-36 ml-4 h-6 truncate ...">
+        <div className="flex absolute text-offWhite text-sm mt-36 ml-4 h-6 truncate ...">
           Assets: {smartContract.assets.length}
-        </div>
-        <div className="flex h-38">
-          <Image src={smartContract.imageUri} className="object-cover h-44 w-64 rounded-xl opacity-95" type="content" />
         </div>
       </div>
     );
