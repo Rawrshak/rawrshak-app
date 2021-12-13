12632b17df685e3f1647f35bf87d89ad1de6b4e3
tbwebb22 <tbwebb22@gmail.com>
2021-12-13 09:32:13 -0500

    Update Hamburger SVG

--- src/assets/icons/Hamburger.tsx
+++ src/assets/icons/Hamburger.tsx
@@ -1,10 +1,10 @@
 function Hamburger() {
   return (
     <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M20.001 12.4443L40.001 12.4443" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M7.9502 24.3936H39.9502" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M20.001 36.4443L40.001 36.4443" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M11 30.4434L5.14142 24.5848C5.06332 24.5067 5.06332 24.38 5.14142 24.3019L11 18.4434" stroke="white" stroke-width="4" stroke-linecap="round" />
+      <path d="M20.001 12.4443L40.001 12.4443" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M7.9502 24.3936H39.9502" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M20.001 36.4443L40.001 36.4443" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M11 30.4434L5.14142 24.5848C5.06332 24.5067 5.06332 24.38 5.14142 24.3019L11 18.4434" stroke="white" strokeWidth="4" strokeLinecap="round" />
     </svg>
   );
 }
